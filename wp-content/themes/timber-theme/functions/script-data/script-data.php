dd5eb21a4bdf7eb14dfc99b8984c7fe5da0cedd6
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2017-09-06 18:22:46 +0300

    renamed the svg image partial for clarity

--- wp-content/themes/timber-theme/functions/script-data/script-data.php
+++ wp-content/themes/timber-theme/functions/script-data/script-data.php
@@ -7,7 +7,7 @@
 function themeprefix_script_data() {
   $script_data = array(
     'ajaxurl' => admin_url( 'admin-ajax.php' ),
-    // 'svg_icon' => Timber::compile_string("{% include 'partials/svg-icon.twig' with {'id': 'icon_id', 'classes': ['c-icon'], 'theme_link': theme_link} %}", array('theme_link' => get_template_directory_uri())),
+    // 'svg_icon' => Timber::compile_string("{% include 'partials/image-svg-sprite.twig' with {'id': 'icon_id', 'classes': ['c-icon'], 'theme_link': theme_link} %}", array('theme_link' => get_template_directory_uri())),
   );
   return $script_data;
 }
