8492c15f80fbe1f1c02a96b14248011981b9fc23
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2017-09-06 18:17:36 +0300

    remove colours and custom css sections from theme customiser

--- wp-content/themes/timber-theme/functions/admin-hooks.php
+++ wp-content/themes/timber-theme/functions/admin-hooks.php
@@ -6,3 +6,14 @@
 
 //Enable background updates even if there is a git in the root (and there is!)
 add_filter( 'automatic_updates_is_vcs_checkout', '__return_false', 1 );
+
+
+
+// Remove unnecessary sections from the theme customizer
+function themeprefix_edit_customizer($wp_customize) {
+  $wp_customize->remove_section("colors");
+  $wp_customize->remove_section("custom_css");
+  // $wp_customize->remove_section("static_front_page");
+}
+
+add_action( "customize_register", "themeprefix_edit_customizer" );
