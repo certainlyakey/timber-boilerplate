a4170e677be131fd02f8bd41149a2d590c5c480e
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2017-10-16 12:54:53 +0300

    Replaced manually defined title tag with the one automatically generated by WP

--- wp-content/themes/timber-theme/functions/theme-setup/theme-support.php
+++ wp-content/themes/timber-theme/functions/theme-setup/theme-support.php
@@ -8,7 +8,11 @@ add_theme_support('post-thumbnails');
 add_theme_support('menus');
 add_theme_support('widgets');
 add_theme_support('custom-header');
-add_theme_support( 'html5', array( 'search-form', 'caption' ) );//Load localization domain
+add_theme_support('title-tag');
+add_theme_support( 'html5', array( 'search-form', 'caption' ) );
+
+
+//Load localization domain
 function themedomain_load_localisation(){
   load_theme_textdomain('themedomain', get_template_directory() . '/languages');
 }
