17f12577eb91a110454666401d19227de162d1b7
AleksandrBeliaev <aleksandr.beliaev@exove.ee>
2017-11-08 21:39:50 +0200

    Added HTML5 gallery support

--- wp-content/themes/timber-theme/functions/theme-setup/theme-support.php
+++ wp-content/themes/timber-theme/functions/theme-setup/theme-support.php
@@ -9,7 +9,7 @@ add_theme_support('menus');
 add_theme_support('widgets');
 add_theme_support('custom-header');
 add_theme_support('title-tag');
-add_theme_support( 'html5', array( 'search-form', 'caption' ) );
+add_theme_support( 'html5', array( 'search-form', 'caption', 'gallery' ) );
 
 
 //Load localization domain
