3bcb907ccc55d2a8075eeabd378ffc1529585e3c
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2020-05-19 02:33:48 +0300

    Remove default Gutenberg block styles and WP Emoji Javascript added by Wordpress to theme code

--- functions/frontend-hooks.php
+++ functions/frontend-hooks.php
@@ -27,3 +27,22 @@ function themeprefix_add_slug_to_body_class( $classes ) {
 }
 
 add_filter( 'body_class', 'themeprefix_add_slug_to_body_class' );
+
+
+
+// Remove WP emoji code
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+
+
+// Remove Gutenberg default styles
+function themeprefix_remove_wp_block_library_css() {
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+}
+
+add_action( 'wp_enqueue_scripts', 'themeprefix_remove_wp_block_library_css', 100 );
